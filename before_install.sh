945dc473e521fb7185dc77eadc6b4320eb7ad1c5
Steffen Lindner <mail@steffen-lindner.de>
2014-07-26 11:51:16 +0200

    mysql only

--- before_install.sh
+++ before_install.sh
@@ -10,7 +10,6 @@ set -e
 
 WORKDIR=$PWD
 CORE_BRANCH=$1
-DB=mysql
 echo "Work directory: $WORKDIR"
 git clone --depth 1 -b $CORE_BRANCH https://github.com/owncloud/core
 cd core
@@ -18,11 +17,10 @@ git submodule update --init
 
 cd $WORKDIR
 
-if [ "$DB" == "mysql" ] ; then
-  mysql -e 'create database oc_autotest;'
-  mysql -u root -e "CREATE USER 'oc_autotest'@'localhost' IDENTIFIED BY 'owncloud'";
-  mysql -u root -e "grant all on oc_autotest.* to 'oc_autotest'@'localhost'";
-fi
+#setup db
+mysql -e 'create database oc_autotest;'
+mysql -u root -e "CREATE USER 'oc_autotest'@'localhost' IDENTIFIED BY 'owncloud'";
+mysql -u root -e "grant all on oc_autotest.* to 'oc_autotest'@'localhost'";
 
 #
 # copy install script
