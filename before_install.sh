3be11ae2374990f88c96f44686e41a770242199a
Steffen Lindner <mail@steffen-lindner.de>
2014-07-26 12:04:40 +0200

    Merge into one file

--- before_install.sh
+++ before_install.sh
@@ -24,8 +24,88 @@ mysql -u root -e "CREATE USER 'oc_autotest'@'localhost' IDENTIFIED BY 'owncloud'
 mysql -u root -e "grant all on oc_autotest.* to 'oc_autotest'@'localhost'";
 
 #
-# copy install script
+# install script
 #
 echo "New Workdir2: $WORKDIR"
 
-bash ./core_install.sh mysql
+DATABASENAME=oc_autotest
+DATABASEUSER=oc_autotest
+ADMINLOGIN=admin
+BASEDIR=$PWD
+
+DBCONFIGS="mysql"
+PHPUNIT=$(which phpunit)
+
+# use tmpfs for datadir - should speedup unit test execution
+DATADIR=$BASEDIR/data-autotest
+
+echo "Using database $DATABASENAME"
+
+cat > ./tests/autoconfig-mysql.php <<DELIM
+<?php
+\$AUTOCONFIG = array (
+  'installed' => false,
+  'dbtype' => 'mysql',
+  'dbtableprefix' => 'oc_',
+  'adminlogin' => '$ADMINLOGIN',
+  'adminpass' => 'admin',
+  'directory' => '$DATADIR',
+  'dbuser' => '$DATABASEUSER',
+  'dbname' => '$DATABASENAME',
+  'dbhost' => 'localhost',
+  'dbpass' => 'owncloud',
+);
+DELIM
+
+function execute_tests {
+    echo "Setup environment for $1 testing ..."
+    # back to root folder
+    cd $BASEDIR
+
+    # revert changes to tests/data
+    #git checkout tests/data/*
+
+    # reset data directory
+    rm -rf $DATADIR
+    mkdir $DATADIR
+ # copy autoconfig
+    cp $BASEDIR/tests/autoconfig-$1.php $BASEDIR/core/config/autoconfig.php
+
+    # trigger installation
+    echo "INDEX"
+    php -f core/index.php
+    echo "END INDEX"
+
+    #test execution
+    cd tests
+    php -f enable_all.php
+}
+
+#
+# start test execution
+#
+if [ -z "$1" ]
+  then
+    # run all known database configs
+    for DBCONFIG in $DBCONFIGS; do
+        execute_tests $DBCONFIG
+    done
+else
+    execute_tests $1 $2 $3
+fi
+
+# show environment
+
+
+echo "owncloud configuration:"
+cat $BASEDIR/config/config.php
+
+
+echo "data directory:"
+ls -ll $DATADIR
+
+echo "owncloud.log:"
+cat $DATADIR/owncloud.log
+
+cd $BASEDIR
+
